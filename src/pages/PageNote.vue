082ee83fedd255de9840d29456e442824c6ce849
Bruno Costa <b.costa@catalogai.com>
2020-10-21 18:08:02 -0300

    Creating PageNote.vue, PageNotes.vue and routes

---
+++ src/pages/PageNote.vue
@@ -0,0 +1,26 @@
+<template>
+  <transition
+        appear
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated fadeOut"
+      >
+      <div class="q-pb-xs q-pt-xs">
+          <h5 class="text-h5 text-weight-bold on-right">Estudos</h5>
+          <q-editor v-model="editor"/>
+          <q-footer bordered>
+              <q-btn to="/note" class="absolute-bottom" dense unelevated size="12px" color="white" text-color="amber" label="Save"/>
+          </q-footer>
+      </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      editor: ''
+    }
+  }
+}
+</script>
+
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/PageNotes.vue') },
+      { path: '/note', component: () => import('pages/PageNote.vue') }
     ]
   },
 

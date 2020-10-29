0b0b98699ff9052386e16d4025a1ca87a59ea5ed
Bruno Costa <b.costa@catalogai.com>
2020-10-29 11:38:28 -0300

    Updating PageNotes.vue

--- src/pages/PageNotes.vue
+++ src/pages/PageNotes.vue
@@ -20,32 +20,7 @@
         </div>
         <span class="hidden">{{ this.$route.params.id }}</span>
         <h5 class="text-h5 text-weight-bold on-right">{{ this.selectedFolder.name}}</h5>
-        <q-list bordered separator>
-          <q-slide-item right-color="red">
-            <template v-slot:right>
-              <q-icon name="delete" />
-            </template>
-            <q-item>
-              <q-item-section side>
-                <q-radio
-                  v-if="showRadioButton"
-                  v-model="radioButtonState"
-                  color="amber"
-                  val="line"
-                />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Estudar Javascript</q-item-label>
-                <q-item-label caption lines="2"
-                  >Preciso lembrar algumas coisas.</q-item-label
-                >
-              </q-item-section>
-              <q-item-section side top>
-                <q-item-label caption>10/10/2020 às 08:32:39</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-slide-item>
-        </q-list>
+        <notes></notes>
         <q-footer align="justify" bordered class="bg-white text-primary">
           <q-btn
             class="float-left on-right text-grey-9"
